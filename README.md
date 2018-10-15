15045881ae72ae22351037124097d620797b9480
Vanessa Sochat <vsochat@stanford.edu>
2018-10-15 18:10:28 -0400

    adding test for travel

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # Emscripten!
 
-This is a tiny repository to learn (and have fun with [Emscripten]().
+This is a tiny repository to learn (and have fun with 
+[Emscripten](http://kripken.github.io/emscripten-site/docs/compiling/WebAssembly.html).
 Specifically, I'm getting the hang of C++ and want to create some "in browser
-experiments."
+experiments." 
+
+## Build
+
+For most of these, you can compile the cpp script as follows:
+
+```bash
+em++ helloworld.cpp -o index.html
+```
+
+If it doesn't work in your browser, you can disable WebAssembly:
+
+```bash
+em++ helloworld.cpp -o index.html -s WASM=0
+```
+
+I was having a hard time getting the wasm to work on Github Pages, so the
+examples here are running without it.
 
 ## Experiments
 
  - [hello-world](hello-world): or more properly, "Hello Dinosaur!" ([web](https://vsoch.github.io/emscripten/hello-world/))
+ - [travel](travel): a fun testing problem I did to study for a course! ([web](https://vsoch.github.io/emscripten/travel/))
---
+++ travel/travel.cpp
@@ -0,0 +1,96 @@
+/** 
+   travel.cpp
+   ----------
+
+   Given a destination point, print out all possible paths through it. 
+
+   @author
+    - Vanessa Sochat
+
+   @references
+    - https://kripken.github.io/emscripten-site/docs/getting_started/Tutorial.html
+
+   @version
+    - October 15, 2018
+
+   @usage
+      em++ travel.cpp
+      node a.out.js
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+/**
+    Function Prototypes
+*/
+
+bool contains(set <string> & seen, string s);
+void findIt(int x, int y, int endX, int endY, set <string> & seen, string path);
+void travel(int x, int y);
+
+
+/**
+    Function Definitions
+*/
+
+
+
+/**
+    Determine if a set contains a particular string. Return True if yes, False
+    otherwise.
+
+    @param seen: a set of strings
+    @param s: the string to find in the set
+*/
+
+bool contains(set <string> & seen, string s) {
+    return seen.find(s) != seen.end();
+}
+
+void findIt(int x, int y, int endX, int endY, set <string> & seen, string path) {
+
+    if (contains(seen, path)) {
+    
+        // We found it, print and add to seen
+        if ((x == endX) && (y == endY)) {
+            cout << path << endl;
+            seen.insert(path);
+
+        // We don't have a solution, keep trying
+        } else {
+        
+            // If we can still move right, do it
+            if (x < endX) {
+                findIt(x+1, y, endX, endY, seen, path + "E ");
+            }
+            // Also try moving up
+            if (y < endY) {
+                findIt(x, y+1, endX, endY, seen, path + "N ");
+            }
+
+            // Also try moving up and right
+            if ((y < endY) && (x < endX)) {
+                findIt(x+1, y+1, endX, endY, seen, path + "NE ");
+            }
+        }
+    }
+}
+
+void travel(int x, int y) {
+
+    set <string> seen;
+    
+    // currentX currentY endX endY
+    findIt(0, 0, x, y, seen, "");
+}
+
+int main() {
+
+    cout << "Let's go on an adventure!" << endl;
+    travel(3, 4);
+    return 0;
+}
